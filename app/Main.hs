c031c967b3667062c61c31a40b9e9ada69fcf788
金沢森 <shin.ne1365@gmail.com>
2025-04-24 23:37:23 +0900

    Haskellの学習開始。難しいなぁ

---
+++ app/Main.hs
@@ -0,0 +1,36 @@
+module Main (main) where
+
+  -- Haskell 1日目
+
+-- IO型の関数
+main :: IO ()
+
+-- do記法を使うと、手続き型パラダイムのような書き方が可能
+main = do
+  -- putStrLnは文字列を表示する関数
+  putStrLn "Hello Haskell!"
+
+  -- 変数の宣言
+  let x :: Int
+      x = 12
+  let y :: Int
+      y = 23
+  let z :: Int
+  
+      z = x * y
+
+  print z
+
+  -- Haskell 2日目
+
+  -- 関数の宣言
+  let square :: Int -> Int
+      -- オフサイドルールがあり、インデントも構文の一部である。
+      square k = k * k
+  print (square 13)
+
+  -- 引数を２つとる関数（内部的には高階関数であり、カリー化により抽象されている）
+  let add :: Int -> Int -> Int
+      add a b = a + b
+
+  print (add 434 871)
